130a17801eb58bae1f6ee60b8357048ae70d6699
djohnson <drjohnson11@winona.edu>
2015-05-02 13:35:37 -0500

    Added files that were missing.

---
+++ SearchGUI/SearchGUIPatientInfoDB.java
@@ -0,0 +1,142 @@
+package MedicalClinicDB.SearchGUI;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+public class SearchGUIPatientInfoDB {
+	protected List<Vector<String>> visits = new ArrayList<>();
+	protected List<String> conditions = new ArrayList<>();
+	protected List<String> devices = new ArrayList<>();
+	protected String patientID = null, clinicNumber = null, name = null,
+			gender = null, birthdate = null, height = null, weight = null,
+			primaryProvider = null, secondaryProvider = null;
+
+	private String className = "com.mysql.jdbc.Driver";
+	private String databaseURL = "jdbc:mysql://cs485project.cloudapp.net:3307/cs485_user";
+	private String databaseUsername = "cs485_user";
+	private String databasePassword = "Databas3";
+	private Connection connection = null;
+	private PreparedStatement ps = null;
+
+	protected void getPatient(int id) throws SQLException {
+		String patientQuery = "SELECT patientID, clinicNumber, firstName, lastName, middleName, gender, birthDate, height, weight\n"
+				+ "FROM Patient\n" + "WHERE patientID = " + id;
+		String primaryProviderQuery = "SELECT hp.firstName, hp.middleName, hp.lastName \n"
+				+ "FROM HealthcareProvider as hp \n"
+				+ "JOIN PatientHealthcareProvider_xref AS phpx \n"
+				+ "ON hp.healthcareProviderID = phpx.healthcareProviderID \n"
+				+ "WHERE phpx.patientID = "
+				+ id
+				+ "\n AND healthcareProviderType = 'Primary'";
+		String secondaryProviderQuery = "SELECT hp.firstName, hp.middleName, hp.lastName \n"
+				+ "FROM HealthcareProvider as hp \n"
+				+ "JOIN PatientHealthcareProvider_xref AS phpx \n"
+				+ "ON hp.healthcareProviderID = phpx.healthcareProviderID \n"
+				+ "WHERE phpx.patientID = "
+				+ id
+				+ "\n AND healthcareProviderType = 'Secondary'";
+		String conditionsQuery = "SELECT c.conditionName \n"
+				+ "FROM `Condition` as c \n"
+				+ "JOIN PatientCondition_xref AS pc \n"
+				+ "ON c.conditionID = pc.conditionID \n"
+				+ "WHERE pc.patientID = " + id;
+		String devicesQuery = "SELECT d.assistiveDeviceName\n"
+				+ "FROM AssistiveDevice as d\n"
+				+ "JOIN PatientAssistiveDevice_xref AS pd \n"
+				+ "ON d.assistiveDeviceID = pd.assistiveDeviceID \n"
+				+ "WHERE pd.patientID = " + id;
+		String visitsQuery = "SELECT v.visitID, v.visitNumber, v.visitDate, v.dateAnalysisComplete, v.dateProcessingComplete, hp.firstName, hp.middleName, hp.lastName \n"
+				+ "FROM Visit as v \n"
+				+ "JOIN HealthcareProvider as hp \n"
+				+ "ON v.healthcareProviderID = hp.healthcareProviderID \n"
+				+ "WHERE v.patientID = " + id;
+
+		connectToDatabase();
+		
+		try {
+			Statement s = connection.createStatement();
+			ResultSet rs = s.executeQuery(patientQuery);
+			
+			if(rs.next()) {
+				patientID = rs.getString("patientID");
+				clinicNumber = rs.getString("clinicNumber");
+				name = rs.getString("lastName") + ", " + rs.getString("firstName") + " " + rs.getString("middleName");
+				gender = rs.getString("gender");
+				birthdate = rs.getString("birthDate");
+				height = rs.getString("height");
+				weight = rs.getString("weight");
+			}
+			
+			System.out.println(primaryProviderQuery);
+			rs = s.executeQuery(primaryProviderQuery);
+			
+			if(rs.next()) {
+				primaryProvider = rs.getString("lastName") + ", " + rs.getString("firstName") + " " + rs.getString("middleName");
+			}
+			
+			rs = s.executeQuery(secondaryProviderQuery);
+			
+			if(rs.next()) {
+				secondaryProvider = rs.getString("lastName") + ", " + rs.getString("firstName") + " " + rs.getString("middleName");
+			}
+			
+			rs = s.executeQuery(conditionsQuery);
+			
+			while(rs.next()) {
+				conditions.add(rs.getString("conditionName"));
+			}
+			
+			rs = s.executeQuery(devicesQuery);
+			
+			while(rs.next()) {
+				devices.add(rs.getString("assistiveDeviceName"));
+			}
+			
+			rs = s.executeQuery(visitsQuery);
+			
+			while(rs.next()) {
+				Vector<String> row = new Vector<>(4);
+				row.add(rs.getString("visitID"));
+				row.add(rs.getString("visitNumber"));
+				row.add(rs.getString("visitDate"));
+				row.add(rs.getString("lastName") + ", " + rs.getString("firstName") + " " + rs.getString("middleName"));
+				visits.add(row);
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			if (ps != null) {
+				ps.close();
+			}
+		}
+
+	}
+
+	public void connectToDatabase() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			connection = DriverManager.getConnection(databaseURL,
+					databaseUsername, databasePassword);
+			System.out.println("Connection Successful");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed");
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ SearchGUI/SearchGUIVisitInfoDB.java
@@ -0,0 +1,163 @@
+package MedicalClinicDB.SearchGUI;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+public class SearchGUIVisitInfoDB {
+	
+	protected List<Vector<String>> treatment = new ArrayList<>();
+	protected List<String> procedure = new ArrayList<>();
+	protected List<String> diagnosis = new ArrayList<>();
+	protected List<String> study = new ArrayList<>();
+	protected List<String> system = new ArrayList<>();
+	protected String visitNumber = null, visitDate = null, provider = null, analysisDate = null,
+					processingDate = null;
+	
+	private String className = "com.mysql.jdbc.Driver";
+	private String databaseURL = "jdbc:mysql://cs485project.cloudapp.net:3307/cs485_user";
+	private String databaseUsername = "cs485_user";
+	private String databasePassword = "Databas3";
+	private Connection connection = null;
+	private PreparedStatement ps = null;
+	
+	
+	protected void getVisit(int id) throws SQLException {
+		String visitQuery = "SELECT v.visitNumber, v.visitDate, v.dateAnalysisComplete, v.dateProcessingComplete, hp.firstName, hp.MiddleName, hp.LastName \n"
+				+ "FROM Visit AS v \n" 
+				+ "JOIN HealthcareProvider AS hp \n"
+				+ "ON hp.healthcareProviderID = v.healthcareProviderID \n"
+				+ "WHERE v.visitID = "
+				+ id;
+		
+		String diagnosisQuery = "SELECT icdd.icd9Code, icdd.icd9Description \n"
+				+ "FROM ICD9Diagnosis AS icdd \n"
+				+ "JOIN VisitDiagnosis_xref AS vdx \n"
+				+ "ON icdd.icd9DiagnosisID = vdx.icd9DiagnosisID \n"
+				+ "WHERE vdx.visitID = "
+				+ id;
+		
+		String studyQuery = "SELECT s.studyID, s.typeOfStudy \n"
+				+ "FROM Study AS s \n"
+				+ "JOIN VisitStudy_xref AS vsx \n"
+				+ "ON s.studyID = vsx.studyID \n"
+				+ "WHERE vsx.visitID = "
+				+ id;
+		
+		String SystemQuery = "SELECT s.systemID, s.systemUsed \n"
+				+ "FROM System AS s \n"
+				+ "JOIN VisitSystem_xref AS vsx \n"
+				+ "ON s.systemID = vsx.systemID \n"
+				+ "WHERE vsx.systemID = "
+				+ id;
+		
+		String treatmentQuery = "SELECT t.treatmentID, t.treatmentName \n"
+				+ "FROM Treatment AS t \n"
+				+ "JOIN VisitTreatment_xref AS vtx \n"
+				+ "ON t.treatmentID = vtx.treatmentID \n"
+				+ "WHERE vtx.visitID = "
+				+ id;
+		
+		connectToDatabase();
+		
+		try {
+			Statement s = connection.createStatement();
+			ResultSet rs = s.executeQuery(visitQuery);
+			
+			if(rs.next()) {
+				visitNumber = rs.getString("visitNumber");
+				provider = rs.getString("lastName") + ", " + rs.getString("firstName") + " " + rs.getString("middleName");
+				visitDate = rs.getString("visitDate");
+				analysisDate = rs.getString("dateAnalysisComplete");
+				processingDate = rs.getString("dateProcessingComplete");
+			}
+			
+			rs = s.executeQuery(diagnosisQuery);
+			
+			while(rs.next()) {
+				diagnosis.add(rs.getString("icd9Code") + " - " + rs.getString(("icd9Description")));
+			}
+			
+			rs = s.executeQuery(studyQuery);
+			
+			while(rs.next()) {
+				study.add(rs.getString("studyID") + " - " + rs.getString(("typeOfStudy")));
+			}
+			
+			rs = s.executeQuery(SystemQuery);
+			
+			while(rs.next()) {
+				system.add(rs.getString("systemID") + " - " + rs.getString(("systemUsed")));
+			}
+			
+			rs = s.executeQuery(treatmentQuery);
+			
+			while(rs.next()) {
+				Vector<String> row = new Vector<>(4);
+				row.add(rs.getString("treatmentID"));
+				row.add(rs.getString("treatmentName"));
+				treatment.add(row);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			if (ps != null) {
+				ps.close();
+			}
+		}
+	}
+	
+	protected void getProcedure(int id) throws SQLException {
+		
+		String procedureQuery = "SELECT icdp.icd9Code, icdp.icd9Description \n"
+				+ "FROM ICD9Procedure AS icdp \n"
+				+ "JOIN TreatmentICD9Procedure_xref AS icdp \n"
+				+ "ON icdp.icd9ProcedureID = icdp.icd9ProcedureID \n"
+				+ "WHERE icdp.treatmentID = "
+				+ id;
+		
+		connectToDatabase();
+		
+		try {
+			Statement s = connection.createStatement();
+			ResultSet rs = s.executeQuery(procedureQuery);
+			
+			while(rs.next()) {
+				procedure.add(rs.getString("icd9Code") + " - " + rs.getString(("icd9Description")));
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			if (ps != null) {
+				ps.close();
+			}
+		}
+	}
+	
+	public void connectToDatabase() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			connection = DriverManager.getConnection(databaseURL,
+					databaseUsername, databasePassword);
+			System.out.println("Connection Successful");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed");
+			e.printStackTrace();
+		}
+	}
+
+
+}
